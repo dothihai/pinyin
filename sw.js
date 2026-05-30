f5ea508a3ab0a798be7e1243a588721e6e0c47ea
dothihai <30550216+dothihai@users.noreply.github.com>
2026-05-30 17:58:52 +0900

    Add sw

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'pinyin-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Cache-first for same-origin; network-first for external (Google Fonts etc.)
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+  );
+});
